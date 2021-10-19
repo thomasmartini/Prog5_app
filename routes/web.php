829707222b4ca86c6d49313f3552f786e0c608ca
thomasmartini <74652979+thomasmartini@users.noreply.github.com>
2021-10-19 18:24:10 +0200

    added forum route and blank blade

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/forum', function () {
+    return view('forum');
 });
 
 Route::get('/greeting', function () {
